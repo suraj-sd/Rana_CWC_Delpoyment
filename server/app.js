0861ea5ff707781d2002612566260573c4813263
Saahil1122 <sg872649@gmail.com>
2026-02-15 11:16:12 +0530

    second commit

--- server/app.js
+++ server/app.js
@@ -2,20 +2,28 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
+
 const app = express();
 
+// 🔹 Middlewares
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cors());
 
+// 🔹 Existing static folders (keep them)
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "views")));
 app.use(express.static(path.join(__dirname, "uploads")));
 
+// 🔹 API routes (keep as-is)
 require("./src/routes/index")(app);
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+// 🔥 Serve React dist (ADD THIS)
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+// 🔥 React fallback (IMPORTANT)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 module.exports = app;
